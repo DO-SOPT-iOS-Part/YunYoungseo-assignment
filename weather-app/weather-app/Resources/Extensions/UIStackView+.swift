77f29aa9752f4d4b0787deb722b4b62c4846af25
0seoYun <yys020819@gmail.com>
2023-10-26 23:05:56 +0900

    Add/1 Extension 추가

--- weather-app/weather-app/Resources/Extensions/UIStackView+.swift
+++ weather-app/weather-app/Resources/Extensions/UIStackView+.swift
@@ -5,4 +5,20 @@
 //  Created by 윤영서 on 10/24/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIStackView {
+    
+    func addArrangedSubviews(_ views: UIView...) {
+        views.forEach {
+            self.addArrangedSubview($0)
+        }
+    }
+    
+    func removeAllArrangedSubviews() {
+        self.arrangedSubviews.forEach {
+            self.removeArrangedSubview($0)
+            $0.removeFromSuperview()
+        }
+    }
+}
--- weather-app/weather-app/Resources/Extensions/UIView+.swift
+++ weather-app/weather-app/Resources/Extensions/UIView+.swift
@@ -5,4 +5,12 @@
 //  Created by 윤영서 on 10/24/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+        func addSubViews(_ views: UIView...) {
+                views.forEach {
+                        self.addSubview($0)
+                }
+        }
+}
