9125a97344b587f7aa8706b477fd8de33ce26daa
0seoYun <yys020819@gmail.com>
2023-10-26 23:06:48 +0900

    Feat/1 리스트 카드뷰 구현

--- weather-app/weather-app/Presentation/List/View/WeatherCardView.swift
+++ weather-app/weather-app/Presentation/List/View/WeatherCardView.swift
@@ -8,116 +8,105 @@
 import UIKit
 import SnapKit
 
-class WeatherCardView: UIView { //물어보자
-    let listBackgroundImage: UIImageView = {
-        let listImage = UIImageView()
-        listImage.image = UIImage(named: "list-image")
-        listImage.layer.cornerRadius = 16
+class WeatherCardView: UIButton {
         
-        return listImage
-    }()
-    
-    let myLocation: UILabel = {
-        let label = UILabel()
-        label.text = "나의 위치"
-        label.font = UIFont.sfBold(size: 24)
-        label.textColor = .white
-        
-        return label
-    }()
-    
-    let currentLocation: UILabel = {
-        let label = UILabel()
-        label.text = "고양시"
-        label.font = UIFont.sfMedium(size: 17)
-        label.textColor = .white
-        
-        return label
-    }()
-    
-    let currentCondition: UILabel = {
-        let label = UILabel()
-        label.text = "흐림"
-        label.font = UIFont.sfMedium(size: 16)
-        label.textColor = .white
-
-        return label
-    }()
-    
-    let maxTemperature: UILabel = {
-        let label = UILabel()
-        label.text = "최고:29°"
-        label.font = UIFont.sfMedium(size: 15)
-        label.textColor = .white
-
-        return label
-    }()
-    
-    let minTemperature: UILabel = {
-        let label = UILabel()
-        label.text = "최저:15°"
-        label.font = UIFont.sfMedium(size: 15)
-        label.textColor = .white
-
-        return label
-    }()
-    
-    let currentTemperature: UILabel = {
-        let label = UILabel()
-        label.text = "21°"
-        label.font = UIFont.sfLight(size: 52)
-        label.textColor = .white
-
-        return label
-    }()
+    let myLocation = UILabel()
+    let currentLocation = UILabel()
+    let currentCondition = UILabel()
+    let maxTemperature = UILabel()
+    let minTemperature = UILabel()
+    let currentTemperature = UILabel()
     
     init() {
         super.init(frame: .zero)
-        addSubview(listBackgroundImage)
-        addSubview(myLocation)
-        addSubview(currentLocation)
-        addSubview(currentCondition)
-        addSubview(maxTemperature)
-        addSubview(minTemperature)
-        addSubview(currentTemperature)
-        
-        listBackgroundImage.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(204)
-            $0.leading.equalToSuperview().offset(20)
-        }
-        
-        myLocation.snp.makeConstraints {
-            $0.top.equalTo(listBackgroundImage).offset(10)
-            $0.leading.equalTo(listBackgroundImage).offset(16)
-        }
-        
-        currentLocation.snp.makeConstraints {
-            $0.top.equalTo(listBackgroundImage).offset(44)
-            $0.leading.equalTo(listBackgroundImage).offset(16)
-        }
-        
-        currentCondition.snp.makeConstraints {
-            $0.bottom.equalTo(listBackgroundImage).offset(-10)
-            $0.leading.equalTo(listBackgroundImage).offset(16)
-        }
-        
-        maxTemperature.snp.makeConstraints {
-            $0.bottom.equalTo(listBackgroundImage).offset(-10)
-            $0.trailing.equalTo(listBackgroundImage).offset(-79)
-        }
-        
-        minTemperature.snp.makeConstraints {
-            $0.bottom.equalTo(listBackgroundImage).offset(-10)
-            $0.trailing.equalTo(listBackgroundImage).offset(-16)
-        }
-        
-        currentTemperature.snp.makeConstraints {
-            $0.top.equalTo(listBackgroundImage).offset(4)
-            $0.trailing.equalTo(listBackgroundImage).offset(-16)
-        }
+        setStyle()
+        setLayout()
+        self.frame = CGRect(x:0, y:0, width: 335, height: 117)
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    
+    func setLayout() {
+        self.addSubViews(myLocation,
+                         currentLocation,
+                         currentCondition,
+                         currentTemperature,
+                         maxTemperature,
+                         minTemperature)
+        
+        myLocation.snp.makeConstraints {
+            $0.top.equalTo(self).offset(10)
+            $0.leading.equalTo(self).offset(16)
+        }
+        
+        currentLocation.snp.makeConstraints {
+            $0.top.equalTo(self).offset(44)
+            $0.leading.equalTo(self).offset(16)
+        }
+        
+        currentCondition.snp.makeConstraints {
+            $0.bottom.equalTo(self).offset(-10)
+            $0.leading.equalTo(self).offset(16)
+        }
+        
+        maxTemperature.snp.makeConstraints {
+            $0.bottom.equalTo(self).offset(-10)
+            $0.trailing.equalTo(self).offset(-79)
+        }
+        
+        minTemperature.snp.makeConstraints {
+            $0.bottom.equalTo(self).offset(-10)
+            $0.trailing.equalTo(self).offset(-16)
+        }
+        
+        currentTemperature.snp.makeConstraints {
+            $0.top.equalTo(self).offset(4)
+            $0.trailing.equalTo(self).offset(-16)
+        }
+    }
+    
+    func setStyle() {
+        self.do {
+            $0.setImage(UIImage(named: "list-image"), for:.normal)
+            $0.layer.cornerRadius = 16
+            $0.isUserInteractionEnabled = true
+        }
+        
+        myLocation.do {
+            $0.text = "나의 위치"
+            $0.font = UIFont.sfBold(size: 24)
+            $0.textColor = .white
+        }
+        
+        currentLocation.do {
+            $0.text = "고양시"
+            $0.font = UIFont.sfMedium(size: 17)
+            $0.textColor = .white
+        }
+        
+        currentCondition.do {
+            $0.text = "흐림"
+            $0.font = UIFont.sfMedium(size: 16)
+            $0.textColor = .white
+        }
+        
+        maxTemperature.do {
+            $0.text = "최고:29°"
+            $0.font = UIFont.sfMedium(size: 15)
+            $0.textColor = .white
+        }
+        minTemperature.do {
+            $0.text = "최저:15°"
+            $0.font = UIFont.sfMedium(size: 15)
+            $0.textColor = .white
+        }
+        currentTemperature.do {
+            $0.text = "21°"
+            $0.font = UIFont.sfLight(size: 52)
+            $0.textColor = .white
+        }
     }
 }
